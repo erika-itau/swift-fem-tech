f3ab54803a5fed4090806035e442a5f7dfd88d4b
Erika Albizzati <ealbizzati@gmail.com>
2020-07-18 20:33:22 -0300

    lista

---
+++ MyApplication-lista/MyApplication/MyApplication/Network/Endpoints.swift
@@ -0,0 +1,18 @@
+//
+//  Endpoints.swift
+//  MyApplication
+//
+//  Created by Erika Albizzati on 18/07/20.
+//  Copyright © 2020 Erika Albizzati. All rights reserved.
+//
+
+import Foundation
+
+struct Endpoints {
+    static let users = "\(API.baseUrl)/users"
+    static let pullRequests = "\(API.baseUrl)/posts/"
+}
+
+struct API {
+    static let baseUrl = "https://jsonplaceholder.typicode.com"
+}
